c73a63e9ad94c692cb73d7e4dad62428d6c44fa5
xxf <xuanyouwu@163.com>
2021-05-13 10:10:26 +0800

    Initial Commit

---
+++ ios_demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ios_demo
+//
+//  Created by XXF on 2021/5/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ios_demo/ios_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_demoApp.swift
+//  ios_demo
+//
+//  Created by XXF on 2021/5/13.
+//
+
+import SwiftUI
+
+@main
+struct ios_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
